28af199b1c3a3b8d735515386125ad624b0f3526
Clayman <Clayman@DESKTOP-DM8E77U>
2020-04-20 13:51:07 +0300

    Color text

--- WinAPI/ModalAndNotModalWindows/resource.h
+++ WinAPI/ModalAndNotModalWindows/resource.h
@@ -7,9 +7,12 @@
 #define IDD_NMODAL                      106
 #define ID_BTN_MODAL                    1001
 #define IDCANCEL                        1002
-#define ID_BTN_NMODAL                   1003
 #define ID_BTN_NOT_MODAL                1003
 #define IDOK                            1004
+#define IDC_COLOR                       1006
+#define IDC_BLACK                       1007
+#define IDC_EDIT2                       1008
+#define IDC_EDIT3                       1009
 
 // Next default values for new objects
 // 
@@ -17,7 +20,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        109
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_CONTROL_VALUE         1010
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
