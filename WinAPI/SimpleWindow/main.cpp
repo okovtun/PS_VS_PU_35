3ad35647d6ca2e59409f160df90ebd0a263cc414
Clayman <Clayman@DESKTOP-DM8E77U>
2020-05-03 13:47:00 +0300

    Simple text editor can open and save files.

--- WinAPI/SimpleWindow/main.cpp
+++ WinAPI/SimpleWindow/main.cpp
@@ -1,8 +1,12 @@
 #include<Windows.h>
 #include"resource.h"
 
+CONST CHAR szFilter[] = "Text files (*.txt)\0*.txt\0All files (*.*)\0*.*\0";
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 BOOL	CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+BOOL	LoadTextFileToEdit(HWND hEdit, LPCTSTR pszFileName);
+BOOL	SaveTextFileFromEdit(HWND hEdit, LPCTSTR pszFileName);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
 {
@@ -72,6 +76,8 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 		hSubMenu = CreatePopupMenu();
 		AppendMenu(hSubMenu, MF_STRING, ID_FILE_NEW, "&New");
+		AppendMenu(hSubMenu, MF_STRING, ID_FILE_OPEN, "&Open");
+		AppendMenu(hSubMenu, MF_STRING, ID_FILE_SAVE, "&Save");
 		AppendMenu(hSubMenu, MF_STRING, ID_FILE_EXIT, "E&xit");
 		AppendMenu(hMenu, MF_STRING | MF_POPUP, (UINT)hSubMenu, "&File");
 
@@ -84,11 +90,86 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		//Adding icons:
 		HICON hIcon = (HICON)LoadImage(NULL, "Document.ico", IMAGE_ICON, 32, 32, LR_LOADFROMFILE);
 		SendMessage(hwnd, WM_SETICON, ICON_BIG, (LPARAM)hIcon);
+
+		//Text editor:
+		RECT rect;
+		GetClientRect(hwnd, &rect);
+
+		HWND hEdit = CreateWindowEx
+		(
+			WS_EX_CLIENTEDGE, "Edit", "",
+			WS_CHILD | WS_VISIBLE | WS_VSCROLL | WS_HSCROLL | ES_MULTILINE | ES_AUTOVSCROLL | ES_AUTOHSCROLL,
+			0, 0, rect.right, rect.bottom,
+			hwnd,
+			(HMENU)IDC_MAIN_EDIT,
+			GetModuleHandle(NULL),
+			NULL
+		);
+
+		if (hEdit == NULL)
+			MessageBox(hwnd, "Con not create edit control", "Error", MB_OK | MB_ICONERROR);
+
+		HFONT hDefault = (HFONT)GetStockObject(DEFAULT_GUI_FONT);
+		SendMessage(hEdit, WM_SETFONT, (WPARAM)hDefault, MAKELPARAM(FALSE, 0));
+
+	}
+	break;
+	case WM_SIZE:
+	{
+		RECT rcClient;
+		GetClientRect(hwnd, &rcClient);
+		HWND hEdit = GetDlgItem(hwnd, IDC_MAIN_EDIT);
+		SetWindowPos(hEdit, NULL, 0, 0, rcClient.right, rcClient.bottom, SWP_NOZORDER);
 	}
 	break;
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
+		case ID_FILE_OPEN:
+		{
+			//Создадим стандартное окно открытия файла:
+			OPENFILENAME ofn;
+			CHAR szFileName[MAX_PATH]{};
+
+			ZeroMemory(&ofn, sizeof(ofn));
+
+			ofn.lStructSize = sizeof(ofn);
+			ofn.hwndOwner = hwnd;
+			ofn.lpstrFilter = szFilter;
+			ofn.lpstrFile = szFileName;
+			ofn.nMaxFile = MAX_PATH;
+			ofn.Flags = OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_HIDEREADONLY;
+			//ofn.lpstrDefExt = "txt";
+
+			if (GetOpenFileName(&ofn))
+			{
+				HWND hEdit = GetDlgItem(hwnd, IDC_MAIN_EDIT);
+				LoadTextFileToEdit(hEdit, szFileName);
+			}
+		}
+		break;
+		case ID_FILE_SAVE:
+		{
+			OPENFILENAME ofn;
+			CHAR szFileName[MAX_PATH]{};
+
+			ZeroMemory(&ofn, sizeof(ofn));
+
+			ofn.lStructSize = sizeof(ofn);
+			ofn.hwndOwner = hwnd;
+			ofn.lpstrFilter = szFilter;
+			ofn.lpstrFile = szFileName;
+			ofn.nMaxFile = MAX_PATH;
+			ofn.Flags = OFN_EXPLORER | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT;
+			ofn.lpstrDefExt = "txt";
+
+			if (GetSaveFileName(&ofn))
+			{
+				HWND hEdit = GetDlgItem(hwnd, IDC_MAIN_EDIT);
+				SaveTextFileFromEdit(hEdit, szFileName);
+			}
+		}
+		break;
 		case ID_FILE_EXIT:
 			DestroyWindow(hwnd);
 			break;
@@ -142,4 +223,57 @@ BOOL	CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	default:return FALSE;
 	}
 	return TRUE;
+}
+
+BOOL	LoadTextFileToEdit(HWND hEdit, LPCTSTR pszFileName)
+{
+	BOOL bSuccess = FALSE;
+
+	HANDLE hFile = CreateFile(pszFileName, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
+	if (hFile != INVALID_HANDLE_VALUE)
+	{
+		DWORD dwFileSize = GetFileSize(hFile, NULL);
+		if (dwFileSize != UINT_MAX)
+		{
+			LPSTR pszFileText = (LPSTR)GlobalAlloc(GPTR, dwFileSize + 1);
+			if (pszFileText)
+			{
+				DWORD dwRead;
+
+				if (ReadFile(hFile, pszFileText, dwFileSize, &dwRead, NULL))
+				{
+					pszFileText[dwFileSize] = 0;
+					if (SetWindowText(hEdit, pszFileText))bSuccess = TRUE;
+				}
+				GlobalFree(pszFileText);
+			}
+		}
+		CloseHandle(hFile);
+	}
+	return bSuccess;
+}
+
+BOOL	SaveTextFileFromEdit(HWND hEdit, LPCTSTR pszFileName)
+{
+	BOOL bSuccess = FALSE;
+	HANDLE hFile = CreateFile(pszFileName, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hFile != INVALID_HANDLE_VALUE)
+	{
+		DWORD dwTextLength = GetWindowTextLength(hEdit);
+		if (dwTextLength > 0)
+		{
+			LPSTR pszText = (LPSTR)GlobalAlloc(GPTR, dwTextLength + 1);
+			if (pszText != NULL)
+			{
+				if (GetWindowText(hEdit, pszText, dwTextLength + 1))
+				{
+					DWORD dwWritten;
+					if (WriteFile(hFile, pszText, dwTextLength, &dwWritten, NULL))bSuccess = TRUE;
+				}
+				GlobalFree(pszText);
+			}
+		}
+		CloseHandle(hFile);
+	}
+	return bSuccess;
 }
--- WinAPI/SimpleWindow/resource.h
+++ WinAPI/SimpleWindow/resource.h
@@ -6,6 +6,8 @@
 #define IDI_ICON1                       102
 #define IDD_DIALOG1                     103
 #define IDD_ABOUT                       103
+#define IDR_MENU2                       105
+#define IDC_MAIN_EDIT                   105
 #define ID_HELP_ABOUT                   40001
 #define ID_FILE_NEW                     40002
 #define ID_FILE_OPEN                    40003
@@ -17,9 +19,9 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_RESOURCE_VALUE        106
 #define _APS_NEXT_COMMAND_VALUE         40007
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
